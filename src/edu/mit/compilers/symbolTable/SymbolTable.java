2fbb31ce7dd96f23ffbac013399fb9f66c841c87
Erastus Murungi <murungi@mit.edu>
2022-03-04 18:16:51 -0500

    added a method to look up an id's descriptorr recursively

--- src/edu/mit/compilers/symbolTable/SymbolTable.java
+++ src/edu/mit/compilers/symbolTable/SymbolTable.java
@@ -1,14 +1,15 @@
 package edu.mit.compilers.symbolTable;
 
 import java.util.HashMap;
+import java.util.Optional;
 
 import edu.mit.compilers.descriptors.Descriptor;
 
 
 public class SymbolTable {
-    public final SymbolTable parent;
-    public final SymbolTableType symbolTableType;
-    public final HashMap<String, Descriptor> entries = new HashMap<>();
+    private final edu.mit.compilers.symbolTable.SymbolTable parent;
+    private final SymbolTableType symbolTableType;
+    private final HashMap<String, Descriptor> entries = new HashMap<>();
 
     public SymbolTable(SymbolTable parent, SymbolTableType symbolTableType) {
         super();
@@ -16,8 +17,27 @@ public class SymbolTable {
         this.symbolTableType = symbolTableType;
     }
 
-    // TODO: looking recursively at parents
+    /**
+     * @param key Look up a variable only within the current scope
+     * @return true if the descriptor for key is found else false
+     */
+
     public boolean containsEntry(String key) {
         return entries.containsKey(key);
+    }
+
+    /**
+     * Look up a variable recursively up the scope hierarchy
+     *
+     * @param stringId the id to lookup in the symbol table hierarchy
+     * @return Optional.empty if the descriptor is not found else Optional[Descriptor]
+     */
+    public Optional<Descriptor> getDescriptorFromValidScopes(String stringId) {
+        Descriptor currentDescriptor = entries.get(stringId);
+        if (currentDescriptor == null) {
+            if (parent != null)
+                return parent.getDescriptorFromValidScopes(stringId);
+        }
+        return Optional.ofNullable(currentDescriptor);
     }
 }
