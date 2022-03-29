6ff8a02cea678f5613a995028ba77d4855700e20
Erastus Murungi <murungi.mit@edu>
2022-03-29 01:05:36 -0400

    some bugs fixes to CFGSymbolTableConverter

--- src/edu/mit/compilers/cfg/CFGSymbolTableConverter.java
+++ src/edu/mit/compilers/cfg/CFGSymbolTableConverter.java
@@ -1,12 +1,15 @@
 package edu.mit.compilers.cfg;
 
-import edu.mit.compilers.descriptors.Descriptor;
-import edu.mit.compilers.descriptors.GlobalDescriptor;
-import edu.mit.compilers.descriptors.MethodDescriptor;
+import edu.mit.compilers.ast.AST;
+import edu.mit.compilers.ast.Block;
+import edu.mit.compilers.ast.Name;
+import edu.mit.compilers.descriptors.*;
 import edu.mit.compilers.symbolTable.SymbolTable;
 import edu.mit.compilers.symbolTable.SymbolTableType;
+import edu.mit.compilers.utils.Pair;
 
 import java.util.HashMap;
+import java.util.Stack;
 import java.util.TreeSet;
 
 public class CFGSymbolTableConverter {
@@ -15,6 +18,7 @@ public class CFGSymbolTableConverter {
     TreeSet<String> imports;
 
     HashMap<String, SymbolTable> cfgMethods = new HashMap<>();
+    int uniqueIndex;
 
     public CFGSymbolTableConverter(GlobalDescriptor globalDescriptor) {
         this.fields = globalDescriptor.globalVariablesSymbolTable;
@@ -27,32 +31,60 @@ public class CFGSymbolTableConverter {
             // is this check necessary? does a method table only contain one entry of type MethodDescriptor?
             if (methodEntry.getValue() instanceof MethodDescriptor) {
                 MethodDescriptor methodDesc = (MethodDescriptor) methodEntry.getValue();
-                SymbolTable methodVars = new SymbolTable(null, SymbolTableType.Method);
+                SymbolTable methodVars = new SymbolTable(null, SymbolTableType.Method, methodDesc.methodDefinition.block);
                 cfgMethods.put(methodEntry.getKey(), methodVars);
 
                 // add all params to symbol table, don't need to check for repeats bc it's the first table
                 methodVars.entries.putAll(methodDesc.parameterSymbolTable.entries);
-
+                methodVars.entries.putAll(this.fields.entries);
                 // iterate through all children of children blocks
-                int uniqueIndex = 1;
-                addChildrenVars(methodVars, methodDesc.localSymbolTable, uniqueIndex);
+                addChildrenVars(methodVars, methodDesc.methodDefinition.block.blockSymbolTable);
+                this.fields.entries.forEach(methodVars.entries::remove);
+                methodVars.parent = this.fields;
             }
         }
+
         return cfgMethods;
     }
 
-    public void addChildrenVars(SymbolTable methodTable, SymbolTable currTable, int uniqueIndex) {
+    public void addChildrenVars(SymbolTable methodTable, SymbolTable currTable) {
         for (HashMap.Entry<String, Descriptor> variable: currTable.entries.entrySet()) {
             // uniquely name each valid variable in method scope
             String varName = variable.getKey();
+            String newVarName = varName;
             if (methodTable.containsEntry(varName)) {
-                varName += "" + uniqueIndex;
-                uniqueIndex += 1;
+                newVarName += uniqueIndex++;
             }
-            methodTable.entries.put(varName, variable.getValue());
+            methodTable.entries.put(newVarName, variable.getValue());
+
+            if (!varName.equals(newVarName)) {
+                currTable.entries.remove(varName);
+                currTable.entries.put(newVarName, variable.getValue());
+                renameVariableWithinCurrentScope(currTable.owner, varName, newVarName);
+            }
         }
 
         for (SymbolTable child : currTable.children)
-            addChildrenVars(methodTable, child, uniqueIndex);
+            addChildrenVars(methodTable, child);
+    }
+
+    private static void renameVariableWithinCurrentScope(Block block, String oldName, String newName) {
+        Stack<AST> toExplore = new Stack<>();
+        toExplore.push(block);
+        while (!toExplore.isEmpty()) {
+            AST ast = toExplore.pop();
+            for (Pair<String, AST> astPair : ast.getChildren()) {
+                AST child = astPair.second();
+                if (child instanceof Name) {
+                    Name name = (Name) child;
+                    if (name.id.equals(oldName)) {
+                        name.id = newName;
+                    }
+                }
+                if (!(child instanceof Block))
+                    toExplore.push(child);
+            }
+        }
+
     }
 }
