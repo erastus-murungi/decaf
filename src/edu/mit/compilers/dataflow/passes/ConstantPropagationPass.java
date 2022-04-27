bb19c48848097a08693d8a52d99f5cced5f69507
Gianna Torpey <gianna.torpey@gmail.com>
2022-04-27 16:23:26 -0400

    Implemented tentatively working constant propagation aha

---
+++ src/edu/mit/compilers/dataflow/passes/ConstantPropagationPass.java
@@ -0,0 +1,99 @@
+package edu.mit.compilers.dataflow.passes;
+
+import edu.mit.compilers.cfg.BasicBlock;
+import edu.mit.compilers.codegen.ThreeAddressCodeList;
+import edu.mit.compilers.codegen.codes.HasOperand;
+import edu.mit.compilers.codegen.codes.HasResult;
+import edu.mit.compilers.codegen.codes.ThreeAddressCode;
+import edu.mit.compilers.codegen.names.AbstractName;
+import edu.mit.compilers.codegen.names.ConstantName;
+import edu.mit.compilers.dataflow.analyses.ReachingDefinitions;
+import edu.mit.compilers.dataflow.operand.UnmodifiedOperand;
+import edu.mit.compilers.dataflow.reachingvalues.DefValue;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConstantPropagationPass extends OptimizationPass {
+
+    public ConstantPropagationPass(Set<AbstractName> globalVariables, BasicBlock entryBlock) {
+        super(globalVariables, entryBlock);
+    }
+
+    public void runGlobalConstantPropagation() {
+        final ReachingDefinitions reachingDefinitions = new ReachingDefinitions(entryBlock);
+        System.out.println("Everything");
+        System.out.println(entryBlock.threeAddressCodeList);
+        for (BasicBlock basicBlock : basicBlocks) {
+            System.out.println("Block before");
+            System.out.println(basicBlock.threeAddressCodeList.getCodes());
+            var block = basicBlock.threeAddressCodeList.getCodes();
+            var in = reachingDefinitions.in.get(basicBlock);
+            var gen = reachingDefinitions.gen(basicBlock);
+            var kill = reachingDefinitions.kill(basicBlock);
+            var out = reachingDefinitions.out.get(basicBlock);
+
+            var tacList = basicBlock.codes();
+            final var newtaclist = basicBlock.threeAddressCodeList;
+            newtaclist.getCodes().clear();
+            var inSet = organize(reachingDefinitions.in.get(basicBlock));
+            HashMap<AbstractName, DefValue> freshlyGen = new HashMap<>();
+            for (ThreeAddressCode line : tacList){
+                if (line instanceof HasResult){
+                    DefValue defValue = new DefValue((HasResult) line);
+                    freshlyGen.put(defValue.variableName, defValue);
+                }
+
+                if (line instanceof HasOperand){
+                    var newLine = (HasOperand) line;
+                    for (AbstractName oldName : newLine.getOperandNames()){
+                        if (inSet.containsKey(oldName) && inSet.get(oldName).size() == 1 && !freshlyGen.containsKey(oldName)){
+                            DefValue defValue = (DefValue) inSet.get(oldName).toArray()[0];
+                            if (defValue.operand instanceof UnmodifiedOperand) {
+                                var unmodified = (UnmodifiedOperand) defValue.operand;
+                                if (unmodified.abstractName instanceof ConstantName){
+                                    var constant = (ConstantName) unmodified.abstractName;
+                                    ((HasOperand) line).replace(oldName, constant);
+                                }
+                            }
+                        } if (freshlyGen.containsKey(oldName)){
+                            DefValue defValue = freshlyGen.get(oldName);
+                            if (defValue.operand instanceof UnmodifiedOperand) {
+                                var unmodified = (UnmodifiedOperand) defValue.operand;
+                                if (unmodified.abstractName instanceof ConstantName){
+                                    var constant = (ConstantName) unmodified.abstractName;
+                                    ((HasOperand) line).replace(oldName, constant);
+                                }
+                            }
+                        }
+                    }
+                }
+                newtaclist.addCode(line);
+            }
+        }
+    }
+
+    private HashMap<AbstractName, Set<DefValue>> organize(Set<DefValue> setOfStuff) {
+        HashMap<AbstractName, Set<DefValue>> labelToKilled = new HashMap<>();
+        for (var stuff : setOfStuff){
+            var label = stuff.variableName;
+            if (labelToKilled.containsKey(label)){
+                labelToKilled.get(label).add(stuff);
+            } else {
+                HashSet<DefValue> placeholder = new HashSet<>();
+                placeholder.add(stuff);
+                labelToKilled.put(label, placeholder);
+            }
+        }
+        return labelToKilled;
+    }
+
+    @Override
+    public boolean run() {
+        final var oldCodes = entryBlock.codes();
+        runGlobalConstantPropagation();
+        return oldCodes.equals(entryBlock.threeAddressCodeList.getCodes());
+    }
+}
