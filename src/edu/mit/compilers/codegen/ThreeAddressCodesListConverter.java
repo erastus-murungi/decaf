425567b680ada5eb3c1a5ba4d09685a6beb31a9f
Erastus M Murungi <murungi@mit.edu>
2022-04-27 16:29:52 -0400

    codegen bug fix

--- src/edu/mit/compilers/codegen/ThreeAddressCodesListConverter.java
+++ src/edu/mit/compilers/codegen/ThreeAddressCodesListConverter.java
@@ -577,9 +577,12 @@ public class ThreeAddressCodesListConverter implements BasicBlockVisitor<ThreeAd
         blockToCodeHashMap.put(basicBlockBranchLess, universalThreeAddressCodeList);
         if (!(basicBlockBranchLess instanceof NOP) && !(basicBlockBranchLess.autoChild instanceof NOP)) {
             if (visited.contains(basicBlockBranchLess.autoChild)) {
-                var label = blockToLabelHashMap.computeIfAbsent(basicBlockBranchLess.autoChild, (key) -> getLabel(basicBlockBranchLess.autoChild, null));
-                universalThreeAddressCodeList.addLast(new UnconditionalJump(label));
-            } else {
+		        var label = blockToLabelHashMap.get(basicBlockBranchLess.autoChild);
+                if (label == null) {
+                    label = getLabel(basicBlockBranchLess.autoChild, null);
+                }
+                universalThreeAddressCodeList.setNext(ThreeAddressCodeList.of(new UnconditionalJump(label)));
+             } else {
                 universalThreeAddressCodeList.setNext(basicBlockBranchLess.autoChild.accept(this, symbolTable));
             }
         } else {
