a0cbc94ade5454230c3c621e01a47d7b48f34a42
Erastus Murungi <erastuswagwan@gmail.com>
2022-05-01 14:23:42 -0400

    renaming changes in main

--- src/edu/mit/compilers/Main.java
+++ src/edu/mit/compilers/Main.java
@@ -3,14 +3,14 @@ package edu.mit.compilers;
 import java.io.FileNotFoundException;
 
 import edu.mit.compilers.tools.CLI;
-import edu.mit.compilers.utils.CompilationController;
+import edu.mit.compilers.utils.Compilation;
 
 class Main {
     public static void main(String[] args) throws FileNotFoundException {
         final String[] optnames = {"cse", "all", "cp"};
         CLI.parse(args, optnames);
 //        String sourceCode = "import printf; \n void main() {int x; x = (1 + 6 * 3); printf(\"%d\", x);}";
-//        new CompilationController(sourceCode).run();
-        new CompilationController().run();
+//        new Compilation(sourceCode).run();
+        new Compilation().run();
     }
 }
