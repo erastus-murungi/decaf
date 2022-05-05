af7c61d90a34ca2939c00eb6c2798f749541aa69
Erastus Murungi <erastuswagwan@gmail.com>
2022-05-04 23:16:04 -0400

    refactoring

--- src/edu/mit/compilers/Main.java
+++ src/edu/mit/compilers/Main.java
@@ -1,9 +1,11 @@
 package edu.mit.compilers;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 import edu.mit.compilers.tools.CLI;
 import edu.mit.compilers.utils.Compilation;
+import edu.mit.compilers.utils.TestRunner;
 
 class Main {
     public static void main(String[] args) throws FileNotFoundException {
@@ -11,6 +13,8 @@ class Main {
         CLI.parse(args, optnames);
 //        String sourceCode = "import printf; \n void main() {int x; x = (1 + 6 * 3); printf(\"%d\", x);}";
 //        new Compilation(sourceCode).run();
+//          new Compilation(new FileInputStream("tests/dataflow/input/cp-10.dcf"), true).run();
         new Compilation().run();
+//        TestRunner.run();
     }
 }
