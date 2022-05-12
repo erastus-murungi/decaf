dc37cfdcf6290f3231a97f516ade21ffdc431d24
Erastus M Murungi <murungi@mit.edu>
2022-05-12 04:28:25 -0400

    fixed cse-08 with regalloc

--- src/edu/mit/compilers/utils/Compilation.java
+++ src/edu/mit/compilers/utils/Compilation.java
@@ -266,7 +266,7 @@ public class Compilation {
             var dataflowOptimizer = new DataflowOptimizer(programIr.methodBeginList, instructionListConverter.globalNames);
             dataflowOptimizer.initialize();
             dataflowOptimizer.optimize();
-            programIr.methodBeginList = dataflowOptimizer.allMethods;
+            programIr.methodBeginList = dataflowOptimizer.toOptimizeMethods;
             nLinesOfCodeReductionFactor = (oldNLinesOfCode - countLinesOfCode()) / oldNLinesOfCode;
             if (CLI.debug) {
                 System.out.println("After optimization");
